7abac34711bd92b0ceec65d183a8f3ea8cb1ef15
Feyxoa <na@feyxoa.com>
2017-10-18 21:19:26 +0400

    create CrimeFragment

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -6,35 +6,6 @@ import java.util.UUID;
 public class Crime {
 
     private UUID mId;
-
-    public UUID getmId() {
-        return mId;
-    }
-
-    public String getmTitle() {
-        return mTitle;
-    }
-
-    public void setmTitle(String mTitle) {
-        this.mTitle = mTitle;
-    }
-
-    public Date getmDate() {
-        return mDate;
-    }
-
-    public void setmDate(Date mDate) {
-        this.mDate = mDate;
-    }
-
-    public boolean ismSolved() {
-        return mSolved;
-    }
-
-    public void setmSolved(boolean mSolved) {
-        this.mSolved = mSolved;
-    }
-
     private String mTitle;
     private Date mDate;
     private boolean mSolved;
@@ -44,4 +15,31 @@ public class Crime {
         mDate = new Date();
     }
 
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
 }
